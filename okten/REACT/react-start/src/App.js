7405c85e390a076b4f475755c79520a189acad19
Raman41k <romangugel@gmail.com>
2022-09-14 15:51:28 +0300

    Clean react app

---
+++ okten/REACT/react-start/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Characters from "./components/Characters/Characters";
+import Hero from "./components/Hero/Hero";
+import Simpson from "./components/Simpsons/Simpson";
+
+function App() {
+  return (
+    <div>
+      {/*<Characters/>*/}
+      {/*<Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>*/}
+      {/*<Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>*/}
+      {/*<Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>*/}
+      {/*<Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>*/}
+      {/*<Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>*/}
+      {/*<Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>*/}
+      {/*<Simpson/>*/}
+    </div>
+  );
+}
+
+export default App;
